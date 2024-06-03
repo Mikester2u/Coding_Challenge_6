a68268f95af3bc5f2de5e6b898bff7065e41d45b
Michael Rueda <michael.a.rueda22@gmail.com>
2024-06-03 14:59:57 -0400

    Implement draw method for Ball class

--- cc_6.js
+++ cc_6.js
@@ -10,5 +10,12 @@ class Ball {
         this.color = color;
     }
 
-    
+
+    draw(context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fillStyle = this.color;
+        context.fill();
+        context.closePath();
+    }
 }
